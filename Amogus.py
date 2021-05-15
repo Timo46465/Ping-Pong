c6a6c96e3a0a8571f0a102090381b656d5d9d275
Timo46465 <84239475+Timo46465@users.noreply.github.com>
2021-05-15 19:36:53 +0300

    v 1.0 in this version im made control of racket

---
+++ Amogus.py
@@ -0,0 +1,23 @@
+class Player():
+    def update_l(self)
+        keys = key.get_pressed()
+        if keys[K_w]and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s]and self.rect.y < win_height -80:
+            self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP]and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN]and self.rect.y < win_height -80:
+            self.rect.y += self.speed
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode(win_width, win_height))
+window.fill(back)
+game = True
+finish = False
+clock = time.clock()
+FPS = 60
+racket1 = Player()
